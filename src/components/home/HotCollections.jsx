30a6ee4dc233fea1e98d4528b3a49d9a6b96adf3
Issac <issaclua@gmail.com>
2024-10-23 21:34:11 -0700

    added responsiveness to the page at 576px

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -35,13 +35,20 @@ const HotCollections = () => {
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 2,
+          slidesToShow: 3,
           slidesToScroll: 2,
           infinite: true,
         },
       },
       {
         breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 576,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
