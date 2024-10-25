28ebacc12f9130c3bef3cd9bf13d774896ce762f
Issac <issaclua@gmail.com>
2024-10-25 09:50:36 -0700

    implemented the API, the arrow slider, skeleton state, and the live countdown timer of NFT

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -1,9 +1,102 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
+import axios from "axios";
+import Slider from "react-slick";
+import './newitems.css'
 
 const NewItems = () => {
+  const [newItems, setNewItems] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://us-central1-nft-cloud-functions.cloudfunctions.net/newItems"
+        );
+        setNewItems(response.data);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const settings = {
+    className: "center",
+    infinite: true,
+    centerPadding: "0px",
+    slidesToShow: 4,
+    swipeToSlide: true,
+    centerMode: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+  };
+
+  const calculateTimeLeft = (expiryDate) => {
+    const difference = new Date(expiryDate).getTime() - new Date().getTime()
+    let timeLeft = {}
+
+    if (difference > 0) {
+      timeLeft = {
+        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+        minutes: Math.floor((difference / 1000 / 60) % 60),
+        seconds: Math.floor((difference /1000) % 60),
+      }
+    }
+
+    return timeLeft
+  }
+
+  const Timer = ({expiryDate}) => {
+    const [timeLeft, setTimeLeft] = useState(calculateTimeLeft(expiryDate))
+
+    useEffect(() => {
+      const timer = setInterval(() => {
+        setTimeLeft(calculateTimeLeft(expiryDate))
+      }, 1000)
+
+      return () => clearInterval(timer)
+    }, [expiryDate])
+
+    if (timeLeft.hours === undefined && timeLeft.minutes === undefined && timeLeft.seconds === undefined) {
+      return null
+    }
+
+    return (
+      <div className="de_countdown">
+        {timeLeft.hours}h {timeLeft.minutes}m {timeLeft.seconds}s
+      </div>
+    )
+  }
+
   return (
     <section id="section-items" className="no-bottom">
       <div className="container">
@@ -14,65 +107,172 @@ const NewItems = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {new Array(4).fill(0).map((_, index) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
-              <div className="nft__item">
-                <div className="author_list_pp">
-                  <Link
-                    to="/author"
-                    data-bs-toggle="tooltip"
-                    data-bs-placement="top"
-                    title="Creator: Monica Lucas"
-                  >
-                    <img className="lazy" src={AuthorImage} alt="" />
-                    <i className="fa fa-check"></i>
-                  </Link>
-                </div>
-                <div className="de_countdown">5h 30m 32s</div>
 
-                <div className="nft__item_wrap">
-                  <div className="nft__item_extra">
-                    <div className="nft__item_buttons">
-                      <button>Buy Now</button>
-                      <div className="nft__item_share">
-                        <h4>Share</h4>
-                        <a href="" target="_blank" rel="noreferrer">
-                          <i className="fa fa-facebook fa-lg"></i>
-                        </a>
-                        <a href="" target="_blank" rel="noreferrer">
-                          <i className="fa fa-twitter fa-lg"></i>
-                        </a>
-                        <a href="">
-                          <i className="fa fa-envelope fa-lg"></i>
-                        </a>
-                      </div>
+          {loading ? (
+            <Slider {...settings}>
+              {[1, 2, 3, 4].map((_, index) => (
+                <div className="nft__item" key={index}>
+                  <div className="author_list_pp skeleton-circle--ni">
+                  <i className="fa fa-check checkmark"></i>
+                  </div>
+                  <div className="skeleton-ni skeleton-image--ni"></div>
+                  <div className="nft__item_info">
+                    <h4>
+                      <div className="skeleton-ni skeleton-text--ni skeleton-title--ni"/>
+                    </h4>
+                    <div className="nft__item_price">
+                      <div className="skeleton-ni skeleton-text--ni skeleton-price--ni" />
+                    </div>
+                    <div className="nft__item_like">
+                      <div className="skeleton-ni skeleton-like--ni" />
                     </div>
                   </div>
-
-                  <Link to="/item-details">
-                    <img
-                      src={nftImage}
-                      className="lazy nft__item_preview"
-                      alt=""
-                    />
-                  </Link>
                 </div>
-                <div className="nft__item_info">
-                  <Link to="/item-details">
-                    <h4>Pinky Ocean</h4>
-                  </Link>
-                  <div className="nft__item_price">3.08 ETH</div>
-                  <div className="nft__item_like">
-                    <i className="fa fa-heart"></i>
-                    <span>69</span>
+              ))}
+            </Slider>
+          ) : (
+            <Slider {...settings}>
+              {newItems.map((item, index) => (
+                <div className="nft__item" key={index}>
+                  <div className="author_list_pp">
+                    <Link
+                      to={`/author/${item.authorId}`}
+                      data-bs-toggle="tooltip"
+                      data-bs-placement="top"
+                      title={`Creator: ${item.authorName}`}
+                    >
+                      <img
+                        className="lazy"
+                        src={item.authorImage}
+                        alt={item.authorName}
+                      />
+                      <i className="fa fa-check"></i>
+                    </Link>
+                  </div>
+                  <Timer expiryDate={item.expiryDate} />
+
+                  <div className="nft__item_wrap">
+                    <div className="nft__item_extra">
+                      <div className="nft__item_buttons">
+                        <button>Buy Now</button>
+                        <div className="nft__item_share">
+                          <h4>Share</h4>
+                          <a href="" target="_blank" rel="noreferrer">
+                            <i className="fa fa-facebook fa-lg"></i>
+                          </a>
+                          <a href="" target="_blank" rel="noreferrer">
+                            <i className="fa fa-twitter fa-lg"></i>
+                          </a>
+                          <a href="">
+                            <i className="fa fa-envelope fa-lg"></i>
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+
+                    <Link to={`/item-details/${item.id}`}>
+                      <img
+                        src={item.nftImage}
+                        className="lazy nft__item_preview"
+                        alt={item.title}
+                      />
+                    </Link>
+                  </div>
+                  <div className="nft__item_info">
+                    <Link to={`/item-details/${item.id}`}>
+                      <h4>{item.title}</h4>
+                    </Link>
+                    <div className="nft__item_price">{item.price} ETH</div>
+                    <div className="nft__item_like">
+                      <i className="fa fa-heart"></i>
+                      <span>{item.likes}</span>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </div>
-          ))}
+              ))}
+            </Slider>
+          )}
         </div>
       </div>
     </section>
+  );
+};
+
+const SampleNextArrow = (props) => {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{
+        ...style,
+        display: "block",
+        right: "-5px",
+        backgroundColor: "white",
+        border: "1px solid black",
+        width: "40px",
+        height: "40px",
+        borderRadius: "50%",
+        zIndex: "1",
+        padding: "5px",
+      }}
+      onClick={onClick}
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="black"
+        width="20px"
+        height="10px"
+        style={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          display: "block",
+        }}
+      >
+        <path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z" />
+      </svg>
+    </div>
+  );
+};
+
+const SamplePrevArrow = (props) => {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{
+        ...style,
+        display: "block",
+        left: "-5px",
+        backgroundColor: "white",
+        border: "1px solid black",
+        width: "40px",
+        height: "40px",
+        borderRadius: "50%",
+        zIndex: "1",
+        padding: "5px",
+      }}
+      onClick={onClick}
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="black"
+        width="20px"
+        height="10px"
+        style={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          display: "block",
+        }}
+      >
+        <path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z" />
+      </svg>
+    </div>
   );
 };
 
