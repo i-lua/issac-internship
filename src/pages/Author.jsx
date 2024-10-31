f6fb0fd2a77898f8d468b5656a957145a2d66e5c
Issac <issaclua@gmail.com>
2024-10-31 09:11:31 -0700

    added skeleton loading state

--- src/pages/Author.jsx
+++ src/pages/Author.jsx
@@ -3,12 +3,13 @@ import AuthorBanner from "../images/author_banner.jpg";
 import AuthorItems from "../components/author/AuthorItems";
 import { Link, useParams } from "react-router-dom";
 import axios from "axios";
+import "./author.css";
 
 const Author = () => {
   const { authorId } = useParams();
   const [authorData, setAuthorData] = useState(null);
-  const [loading, setLoading] = useState(true)
-  const [isFollowing, setIsFollowing] = useState(false)
+  const [loading, setLoading] = useState(true);
+  const [isFollowing, setIsFollowing] = useState(false);
 
   useEffect(() => {
     const fetchAuthorData = async () => {
@@ -20,19 +21,65 @@ const Author = () => {
       } catch (error) {
         console.error("Error fetching author data:", error);
       } finally {
-        setLoading(false)
+        setLoading(false);
       }
     };
 
-    fetchAuthorData()
+    fetchAuthorData();
   }, [authorId]);
 
   const handleFollowToggle = () => {
-    setIsFollowing((prev) => !prev)
-  }
+    setIsFollowing((prev) => !prev);
+  };
 
   if (loading) {
-    return <p>Loading...</p>
+    return (
+      <div className="main-skeleton">
+        <div className="skeleton-a skeleton-items-a"></div>
+        <div className="row-a">
+          <div className="skeleton-profile-a">
+            <div className="skeleton-a skeleton-avatar"></div>
+            <div className="skeleton-text-group">
+              <div className="skeleton-a skeleton-text-a"></div>
+              <div className="skeleton-a skeleton-text-a"></div>
+              <div className="skeleton-a skeleton-text-a"></div>
+            </div>
+          </div>
+          <div className="skeleton-btn-container">
+            <div className="skeleton-a skeleton-text-a"></div>
+            <div className="skeleton-a skeleton-button-a"></div>
+          </div>
+        </div>
+        {[1, 2, 3, 4, 5, 6, 7, 8].map((_, index) => (
+          <div
+            className="d_item col-lg-3 col-md-6 col-sm-6 col-xs-12"
+            style={{
+              display: "inline-flex",
+              backgroundSize: "cover",
+              flexDirection: "column",
+            }}
+            key={index}
+          >
+            <div className="author_list_pp skeleton-circle--ni">
+              <i className="fa fa-check checkmark"></i>
+            </div>
+            <div className="skeleton-ni skeleton-image--ni"></div>
+            <div className="nft__item_info">
+              <h4>
+                <div className="skeleton-ni skeleton-text--ni skeleton-title--ni" />
+              </h4>
+              <div className="nft__item_price">
+                <div className="skeleton-ni skeleton-text--ni skeleton-price--ni" />
+              </div>
+              <div className="nft__item_like">
+                <div className="skeleton-ni skeleton-like--ni" />
+              </div>
+            </div>
+          </div>
+        ))}
+        ;
+      </div>
+    );
   }
 
   return (
@@ -61,7 +108,9 @@ const Author = () => {
                       <div className="profile_name">
                         <h4>
                           {authorData.authorName}
-                          <span className="profile_username">@{authorData.tag}</span>
+                          <span className="profile_username">
+                            @{authorData.tag}
+                          </span>
                           <span id="wallet" className="profile_wallet">
                             {authorData.address}
                           </span>
@@ -77,8 +126,12 @@ const Author = () => {
                       <div className="profile_follower">
                         {authorData.followers + (isFollowing ? 1 : 0)} followers
                       </div>
-                      <Link to="#" onClick={handleFollowToggle} className="btn-main">
-                        {isFollowing ? 'Unfollow' : 'Follow'}
+                      <Link
+                        to="#"
+                        onClick={handleFollowToggle}
+                        className="btn-main"
+                      >
+                        {isFollowing ? "Unfollow" : "Follow"}
                       </Link>
                     </div>
                   </div>
@@ -87,7 +140,10 @@ const Author = () => {
 
               <div className="col-md-12">
                 <div className="de_tab tab_simple">
-                  <AuthorItems items={authorData.nftCollection} authorData={authorData} />
+                  <AuthorItems
+                    items={authorData.nftCollection}
+                    authorData={authorData}
+                  />
                 </div>
               </div>
             </div>
